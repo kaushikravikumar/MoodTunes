e2eb65382ab07af64a6ca946db2615f15ed3bfb3
Kaushik Ravikumar <kaushik.ravikumar1999@gmail.com>
2018-06-22 09:49:34 -0700

    added pubnub functions

---
+++ playmusic-function.js
@@ -0,0 +1,69 @@
+export default (request, response) => {
+
+    const xhr = require('xhr'); // xhr module to make HTTP requests
+
+    var speech_input = JSON.parse(request.body).queryResult.queryText; // User's speech input
+
+    const amazon_url = "https://pubsub.pubnub.com/v1/blocks/sub-key/sub-c-1f19060e-6a91-11e8-9683-aecdde7ceb31/amazoncomprehend"; // url to amazon comprehend function
+
+    // Object with amazon comprehend function request parameters
+    const amazon_request_options = {
+        "method": "POST",
+        "body": {
+            "speechInput": speech_input,
+            "comprehend": {
+                "language": "en",
+                "location": "speechInput"
+            }
+        }
+    };
+    return xhr.fetch(amazon_url, amazon_request_options).then((sentiment_response) => {
+        var positive_measure = JSON.parse(sentiment_response.body).sentiment.SentimentScore.Positive; // Number from 0 to 1 representing how positive user's speech input was.
+        var song_url;
+
+        // Really happy song
+        if (positive_measure > 0.75) {
+            song_url = "https://github.com/kaushikravikumar/MoodTunes/blob/master/music_files/reallypositive.mp3?raw=true";
+        }
+        // Slightly happy song
+        else if (positive_measure > 0.5) {
+            song_url = "https://github.com/kaushikravikumar/MoodTunes/blob/master/music_files/slightlypositive.mp3?raw=true";
+        }
+        // Slightly sad song
+        else if (positive_measure > 0.25) {
+            song_url = "https://github.com/kaushikravikumar/MoodTunes/blob/master/music_files/slightlynegative.mp3?raw=true";
+        }
+        // Really sad song
+        else {
+            song_url = "https://github.com/kaushikravikumar/MoodTunes/blob/master/music_files/reallynegative.mp3?raw=true";
+        }
+        // Stringified JSON response sent to Dialogflow, which contains instructions on playing song.
+        response.send(JSON.stringify({
+            "payload": {
+                "google": {
+                    "expectUserResponse": false,
+                    "richResponse": {
+                        "items": [{
+                                "simpleResponse": {
+                                    "textToSpeech": "Playing song!"
+                                }
+                            },
+                            {
+                                "mediaResponse": {
+                                    "mediaType": "AUDIO",
+                                    "mediaObjects": [{
+                                        "name": "Song",
+                                        "description": "Based on your mood",
+                                        "contentUrl": song_url
+                                    }]
+                                }
+                            }
+                        ],
+                        "suggestions": []
+                    }
+                }
+            }
+        }));
+        return response;
+    });
+};
